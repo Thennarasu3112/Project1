72af98d4ef178db588f2fa981bef3a6f64e87af5
Thennarasu3112 <thennarasu3112@gmail.com>
2023-08-05 11:45:21 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+import numpy as np
+import pandas as pd
+import cx_Oracle
+import base64
+import logging
+
+
+def DB_Connect():
+    #write your code here
+    pass
+    
+if __name__ == "__main__":
+    try:
+        DB_connect()
+    except Exception as e:
+        Print('Data Base Error Occured:',str(e)
